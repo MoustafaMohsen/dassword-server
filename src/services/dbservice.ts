7ff58f533c038f9765dc2785294a6be0d51c6d62
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-03-04 21:16:59 +0200

    DBServices DB interface functions

--- src/services/dbservice.ts
+++ src/services/dbservice.ts
@@ -1,4 +1,4 @@
-import { Client, ClientConfig } from "pg";
+import { Client, ClientConfig, QueryConfig } from "pg";
 
 export class DBService {
     dbsettings: ClientConfig = {
@@ -79,5 +79,160 @@ export class DBService {
         return result;
     }
 
+    
+    // Query helpers ==========
+    async insertRows(tabelname, client: Client, cols: string[], values: string[][]) {
+        const queries = this.create_multiple_insert_queries(tabelname, cols, values);
+        // callback
+        let done = 0;
+        new Promise((resolve, reject) => {
+            for (let i = 0; i < queries.length; i++) {
+                const q = queries[i];
+                // promsies.push(client.query(q));
+                client.query(q).then((results) => {
+                    let t = q;
+                    // console.log(q); 
+                    done++;
+                    if (done == queries.length) {
+                        resolve(true);
+                    }
+
+                },
+                    (err) => {
+                        console.error(err);
+                        console.log(q);
+                        reject(err)
+                    })
+            }
+        })
+    }
+
+    create_multiple_insert_queries(tabelname, cols: string[], values_array: string[][]) {
+        const queries: QueryConfig[] = [];
+        for (let i = 0; i < values_array.length; i++) {
+            const values = values_array[i]
+            const query = this.create_insert_query(tabelname, cols, values);
+            queries.push(query);
+        }
+        return queries;
+    }
+
+    create_select_query(tablename, cols: string[] | string, values: {}, relation: "OR" | "AND"): QueryConfig {
+        let equals_keys = Object.keys(values);
+        let equals = Object.values(values);
+        let _tmp_keys = equals_keys ? "WHERE " : "";
+        for (let i = 0; i < equals_keys.length; i++) {
+            const key = equals_keys[i];
+            const value = values[key];
+            _tmp_keys = _tmp_keys + key + `=$${i + 1} ` + (i != equals_keys.length - 1 ? relation : "");
+        }
+        let _tmp_cols = cols ? typeof cols == "string" ? cols : cols.join(", ") : "*";
+        const query = {
+            text: `SELECT  ${_tmp_cols} FROM ${tablename} ${_tmp_keys}`,
+            values: equals
+        }
+        return query;
+    }
+
+    create_delete_query(tablename, cols: string[] | string, values: {}, relation: "OR" | "AND"): QueryConfig {
+        let equals_keys = Object.keys(values);
+        let equals = Object.values(values);
+        let _tmp_keys = equals_keys ? "WHERE " : "";
+        for (let i = 0; i < equals_keys.length; i++) {
+            const key = equals_keys[i];
+            const value = values[key];
+            _tmp_keys = _tmp_keys + key + `=$${i + 1} ` + (i != equals_keys.length - 1 ? relation : "");
+        }
+        let _tmp_cols = cols ? typeof cols == "string" ? cols : cols.join(", ") : "*";
+        const query = {
+            text: `DELETE  ${_tmp_cols} FROM ${tablename} ${_tmp_keys}`,
+            values: equals
+        }
+        return query;
+    }
+
+    create_insert_query(tabelname, cols: string[], values: string[]): QueryConfig {
+        let _tmp_cols_arr = [];
+        for (let i = 0; i < cols.length; i++) {
+            _tmp_cols_arr.push("$" + (i + 1));
+        }
+        let _tmp_val_replace = _tmp_cols_arr.join(", ");
+        let _tmp_cols = cols.map(d => d.replace("'", "''")).join(", ");
+        const query = {
+            text: `INSERT INTO ${tabelname} (${_tmp_cols}) VALUES(${_tmp_val_replace}) RETURNING contact_reference_id`,
+            values
+        }
+        return query;
+    }
+
+    create_update_query(tabelname, object: object, condition, relation: "OR" | "AND" = "AND"): QueryConfig {
+
+        let equals_keys = Object.keys(object);
+        let equals = Object.values(object);
+        let _set_string = "";
+        var last = 0;
+        for (let i = 0; i < equals_keys.length; i++) {
+            const key = equals_keys[i];
+            const value = object[key];
+            _set_string = _set_string + key + `=$${i + 1} ` + (i != equals_keys.length - 1 ? ", " : "");
+            last = i;
+        }
+        last++;
+
+        let cond_keys = Object.keys(condition);
+        let cond = Object.values(condition);
+        let _where_string = cond_keys ? "WHERE " : "";
+        for (let i = 0; i < cond_keys.length; i++) {
+            const key = cond_keys[i];
+            const value = condition[key];
+            _where_string = _where_string + key + `=$${i + 1 + last} ` + (i != cond_keys.length - 1 ? relation + " " : "");
+        }
+
+        const query = {
+            text: `UPDATE ${tabelname} SET ${_set_string} ${_where_string}`,
+            values: equals.concat(cond)
+        }
+        return query;
+    }
+
+    // Query helpers ==========
+
+    async insert_object(data: object, tabelname, dbname = "dassworddb") {
+        let keys = Object.keys(data);
+        let values = Object.values(data);
+        const query = this.create_insert_query(tabelname, keys, values);
+        const client = await this.connect(dbname);
+        let result = await client.query(query);
+        await client.end();
+        return result;
+    }
+
+    async update_object<T = any>(data: object, condition: object, tabelname, dbname = "dassworddb") {
+        const query = this.create_update_query(tabelname, data, condition);
+        const client = await this.connect(dbname);
+        let result = await client.query<T>(query);
+        await client.end();
+        return result;
+    }
+
+    async get_object<T = any>(data: object, relation: "OR" | "AND", tabelname, dbname = "dassworddb") {
+        let keys = Object.keys(data)[0];
+        let values = Object.values(data)[0];
+        const query = this.create_select_query(tabelname, keys, values, relation);
+        const client = await this.connect(dbname);
+        let result = await client.query<T>(query);
+        await client.end();
+        return result;
+    }
+
+    async delete_object<T = any>(data: object, relation: "OR" | "AND", tabelname, dbname = "dassworddb") {
+        let keys = Object.keys(data)[0];
+        let values = Object.values(data)[0];
+        const query = this.create_delete_query(tabelname, keys, values, relation);
+        const client = await this.connect(dbname);
+        let result = await client.query<T>(query);
+        await client.end();
+        return result;
+    }
 
 }
