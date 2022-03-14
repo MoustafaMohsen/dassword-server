1fc1733bc09e5115555aa9045da90dedea5d81a9
Moustafa Mohsen <moustafamohsen1@gmail.com>
2022-03-14 08:47:46 +0200

    API endpoints for REST user IPFS Store

--- src/server/server-routes.ts
+++ src/server/server-routes.ts
@@ -1,8 +1,12 @@
+import { IDBUser } from './../interfaces/user.interface';
 import { Web3Store } from './../services/web3-storage';
 import performance from "perf_hooks";
 import express from "express";
 import MainServerCore from './core/server-core';
 import { DBService } from "../services/dbservice";
+import { UserService } from '../services/user.service';
+import * as fs from 'fs';
+import * as stream from 'stream';
 
 export default class MainServerRoutes extends MainServerCore {
 
@@ -49,7 +53,7 @@ export default class MainServerRoutes extends MainServerCore {
             }
         })
 
-        this.app.post('/admin/prepare-db/' + process.env.APP_SECRET_KEY, async (req, res) => {
+        this.app.post('/admin/prepare-db' + process.env.APP_SECRET_KEY, async (req, res) => {
             let t0 = performance.performance.now();
             let data = {} as any;
             const db = new DBService();
@@ -61,6 +65,54 @@ export default class MainServerRoutes extends MainServerCore {
             }
         })
         //#endregion
+
+
+
+        this.app.post('/create-db-user', async (req, res) => {
+            let t0 = performance.performance.now();
+            try {
+                const userSrv = new UserService();
+                let body: IDBUser = req.body;
+                userSrv.create_db_user(body).then((d) => {
+                    send(res, d, t0)
+                }).catch(e => {
+                    err(res, e, t0)
+                })
+            } catch (error) {
+                err(res, error, t0)
+            }
+        })
+
+
+        this.app.post('/get-db-user', async (req, res) => {
+            let t0 = performance.performance.now();
+            try {
+                const userSrv = new UserService();
+                let body: IDBUser = req.body;
+                userSrv.get_db_user(body).then((d) => {
+                    send(res, d, t0)
+                }).catch(e => {
+                    err(res, e, t0)
+                })
+            } catch (error) {
+                err(res, error, t0)
+            }
+        })
+
+        this.app.post('/delete-db-user', async (req, res) => {
+            let t0 = performance.performance.now();
+            try {
+                const userSrv = new UserService();
+                let body: IDBUser = req.body;
+                userSrv.delete_db_user(body).then((d) => {
+                    send(res, d, t0)
+                }).catch(e => {
+                    err(res, e, t0)
+                })
+            } catch (error) {
+                err(res, error, t0)
+            }
+        })
 
 
         this.app.post('/ipfs/list-all-files/' + process.env.APP_SECRET_KEY, async (req, res) => {
@@ -95,7 +147,15 @@ export default class MainServerRoutes extends MainServerCore {
                 if (!req.files || Object.keys(req.files).length === 0) {
                     return err(res, 'No files were uploaded.', t0);
                 }
-                data = await web3.storeFiles(req.files.encrypteddb);
+
+                const buffer = Buffer.from(req.files.encrypteddb['data']);
+                const theStream = ()=>stream.Readable.from(buffer);
+
+                let node_file:any = {
+                    name:"test",
+                    stream:theStream
+                }
+                data = await web3.storeFiles(node_file);
                 send(res, data, t0)
             } catch (error) {
                 err(res, error, t0)
